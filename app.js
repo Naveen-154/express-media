d38fc5dc116c7c5b5b81d2dd6a97a73274ccea0f
Naveen-154 <sonspn23@gmail.com>
2024-04-05 08:48:51 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express=require('express')
+const  app = express()
+const PORT =3500
+app.get('/',(request,response)=>{
+    response.status(200).send({message:'server running'})
+})
+app.listen(PORT,console.log('server running http://localhost:3500'))
